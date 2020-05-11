2de942fe135f3f9579e1d3d4fbd7d8ad9107c600
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 15:38:59 +0800

    实现signup接口，可以注册

--- app/api.py
+++ app/api.py
@@ -11,3 +11,18 @@ db.initial()
 # 用命令 python -c 'import os; print(os.urandom(24))' 生成
 app.secret_key = '\xb9dLy\xbc\xe4\xd5D\x04s8\xfei\xf377i\x17~\x08_9)R'
 
+
+@app.route('/user/signup/', methods=['POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.args.get('username')
+        password = request.args.get('password')
+        if username is not None and password is not None and username != "" and password != "":
+            message = db.insert(username, password)
+        else:
+            message = "请输入合法的用户名和密码！"
+        return jsonify(msg=message, username=username, password=password)
+
+
+if __name__ == '__main__':
+    app.run()
