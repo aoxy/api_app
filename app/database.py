35267769d5332084a29fe630b8d0b3527a7e7b8b
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 15:36:52 +0800

    添加操作数据库的insert()，向数据库插入不重复用户名和加密的密码

--- app/database.py
+++ app/database.py
@@ -19,3 +19,23 @@ def initial():
         con.rollback()
     finally:
         con.close()
+
+
+def insert(username, password):
+    '''
+    注册用户，用户名未占用的才能注册，把密码加密后再存入数据库
+    '''
+    try:
+        with sql.connect("api.db") as con:
+            cur = con.cursor()
+            cur.execute(
+                "INSERT INTO user(USERNAME, ENC_PASSWORD) VALUES(?, ?)",
+                (username, generate_password_hash(password)))
+            con.commit()
+            msg = '注册成功！'
+    except BaseException:
+        con.rollback()
+        msg = '该用户名已被占用，注册失败！'
+    finally:
+        con.close()
+        return msg
