6a6e46edb4424bb7ecde6fd6719fef417e142a98
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 17:18:52 +0800

    在utilit.py中实现token验证函数

--- app/utilit.py
+++ app/utilit.py
@@ -14,3 +14,20 @@ def create_token(username):
     # 接收用户名、转换与编码
     token = s.dumps({"username": username}).decode("ascii")
     return token
+
+
+def verify_token(token):
+    '''
+    校验token，返回用户名
+    '''
+    # 参数为私有秘钥，跟上面方法的秘钥保持一致
+    s = Serializer(current_app.config["SECRET_KEY"])
+    try:
+        # 转换为字典
+        data = s.loads(token)
+    except Exception:
+        return None
+    # 拿到转换后的数据，根据模型类去数据库查询用户信息
+    username = db.query_login(data["username"])
+    return username
+
