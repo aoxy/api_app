0a4f7b90ad6db663bc71f242b71042041277c7a4
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 15:34:45 +0800

    添加操作数据库的initial()，建表，初始化数据库

--- app/api.py
+++ app/api.py
@@ -1,5 +1,13 @@
 import time
-from flask import Flask, redirect, url_for, request, render_template, json, make_response, session
+from flask import Flask, redirect, url_for, request, render_template, jsonify, make_response, session
 
-#操作数据库
+# 操作数据库
 import database as db
+
+
+app = Flask(__name__)
+# 初始化，建表
+db.initial()
+# 用命令 python -c 'import os; print(os.urandom(24))' 生成
+app.secret_key = '\xb9dLy\xbc\xe4\xd5D\x04s8\xfei\xf377i\x17~\x08_9)R'
+
--- app/database.py
+++ app/database.py
@@ -1,2 +1,21 @@
 import sqlite3 as sql
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import request, jsonify, current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+def initial():
+    '''
+    初始化数据库，创建一个表user存用户名和加密过的密码
+    '''
+    try:
+        with sql.connect("api.db") as con:
+            cur = con.cursor()
+            cur.execute("DROP TABLE IF EXISTS user")
+            cur.execute(
+                "CREATE TABLE user(USERNAME varchar(32) PRIMARY KEY, ENC_PASSWORD varchar(128) NOT NULL)")
+            con.commit()
+    except BaseException:
+        con.rollback()
+    finally:
+        con.close()
