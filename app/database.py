b8a524ca45f44ae0fb022198adcae0fb4e6dc66a
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 15:47:43 +0800

    实现生成token的函数

--- app/database.py
+++ app/database.py
@@ -4,6 +4,21 @@ from flask import request, jsonify, current_app
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 
 
+def create_token(username):
+    '''
+    生成token
+    :param username:用户名
+    :return: token
+    '''
+
+    # 第一个参数是内部的私钥
+    # 第二个参数是有效期(秒)
+    s = Serializer(current_app.config["SECRET_KEY"], expires_in=3600)
+    # 接收用户名、转换与编码
+    token = s.dumps({"username": username}).decode("ascii")
+    return token
+
+
 def initial():
     '''
     初始化数据库，创建一个表user存用户名和加密过的密码
