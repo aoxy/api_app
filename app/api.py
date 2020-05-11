b8e65c471dcf79d8533daeb7de31b854ec442061
敖旭扬 <jerryao@mail.ustc.edu.cn>
2020-05-11 17:30:56 +0800

    添加date接口

--- app/api.py
+++ app/api.py
@@ -3,7 +3,7 @@ from flask import Flask, redirect, url_for, request, render_template, jsonify, m
 
 # 操作数据库
 import database as db
-
+from utilit import create_token, login_required, verify_token, festival
 
 app = Flask(__name__)
 # 初始化，建表
@@ -34,6 +34,20 @@ def login():
             return jsonify(msg=message, username=username)
         else:
             return jsonify(msg=message, username=username, token=token)
+
+
+@app.route('/date/', methods=['POST'])
+@login_required # 必须登录的装饰器校验
+def date():
+    if request.method == 'POST':
+        token = request.args.get('token')
+        date = request.args.get('date')
+        username = verify_token(token)
+        if username is None:
+            return jsonify(msg="请先登陆！")
+        else:
+            message = festival(date)
+            return jsonify(msg=message) if message != "" else jsonify()
 
 
 if __name__ == '__main__':
